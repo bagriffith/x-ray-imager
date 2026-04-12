61a9856e0389a18687dde0dbc14fd848952cf4f4
Brady Griffith <bradyagriffith@gmail.com>
2026-04-11 20:57:05 -0700

    Added DBSCAN with amplitude KMeans fallback cluster algorithm.

--- src/x_ray_imager_bagriff/identify_lines/_cluster.py
+++ src/x_ray_imager_bagriff/identify_lines/_cluster.py
@@ -22,3 +22,48 @@
 
 TODO Full description.
 """
+from typing import Optional
+import logging
+from sklearn.base import ClusterMixin
+from sklearn.cluster import DBSCAN, KMeans
+import numpy as np
+
+
+class DBSCANFallbackKMeans(ClusterMixin):
+    def __init__(self,
+                 min_clusters: int,
+                 dbscan_kwargs: Optional[dict] = None,
+                 kmeans_kwargs: Optional[dict] = None
+                 ) -> None:
+        self.min_clusters = min_clusters
+
+        if dbscan_kwargs is None:
+            dbscan_kwargs = {'metric': 'canberra'}
+
+        if kmeans_kwargs is None:
+            kmeans_kwargs = {'n_init': 32}
+
+        self.dbscan_cluster = DBSCAN(**dbscan_kwargs)
+        self.kmeans_cluster = KMeans(self.min_clusters, **kmeans_kwargs)
+        super().__init__()
+
+    def fit(self, x: np.typing.ArrayLike):
+        x = np.array(x, dtype=np.float64)
+        self.labels_ = self.dbscan_cluster.fit(x).labels_
+
+        # Only positive labels are clusters.
+        # -1 marks noisy (background) points.
+        in_cluster = self.labels_ >= 0
+        n_clusters = len(set(self.labels_[in_cluster]))
+        logging.info('DBSCAN found %s clusters.', n_clusters)
+
+        # Split into enough clusters with KMeans
+        if n_clusters < self.min_clusters:
+            logging.info('Min of %s clusters requested. Applying K-means.',
+                         self.min_clusters)
+            amplitude = np.sum(x[in_cluster], axis=1).reshape(-1, 1)
+            kmeans_fit = self.kmeans_cluster.fit(amplitude)
+            self.labels_[in_cluster] = kmeans_fit.labels_
+            n_clusters = self.min_clusters
+
+        return self
