894541cddc5a798234bbe8880422fe41ee618912
Brady Griffith <bradyagriffith@gmail.com>
2026-04-18 15:03:12 -0700

    Added cluster test.

---
+++ tests/identify_lines/test_cluster.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2026 Brady Griffith
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# import pytest
+import numpy as np
+from x_ray_imager_bagriff.identify_lines import DBSCANFallbackKMeans
+
+
+def test_dbscan_kmeans():
+    n_points = 10_000
+    means = (256, 512)
+    np.random.seed(0)  # Keep the set consistent between tests
+    example_set = np.concat(
+        [np.random.poisson(x, size=(n_points, 4)) for x in means]
+    )
+
+    correct_labels = np.repeat(np.arange(len(means)), n_points)
+    cluster = DBSCANFallbackKMeans(2)
+    identified_cluster = cluster.fit_predict(example_set)
+    assert np.all(correct_labels == identified_cluster)
