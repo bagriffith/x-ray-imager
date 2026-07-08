e624398bb5c2b99e6cec0d415d74c6f65b2630e7
Brady Griffith <bradyagriffith@gmail.com>
2026-07-07 22:42:10 -0700

    Added docstring to anger position estimator file.

--- src/x_ray_imager_bagriff/position_estimation/_anger.py
+++ src/x_ray_imager_bagriff/position_estimation/_anger.py
@@ -17,6 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+"""Anger imager style portioning algorithms."""
 import logging
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
